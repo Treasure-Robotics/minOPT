1fc407b9d0922657a6669b95f85878c9c931e6ee
Benjamin Bolte <ben@bolte.cc>
2022-09-13 23:21:21 -0400

    min-opt -> minopt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="min-opt",
+    name="minopt",
     version="0.0.1",
     description="MinOPT model repository",
     author="Benjamin Bolte",
